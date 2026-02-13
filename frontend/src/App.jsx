b89548f1aa5021305e24678e05641eb5bd107242
Krish-Guatam <krishgutam15@gmail.com>
2026-02-13 16:10:21 +0530

    added an responsive navbar in component/layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from "./components/layout/Navbar";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-      </h1>
+      <div className="min-h-screen bg-[#0d0d0d] text-white">
+        <div className='fixed w-full mt-6'>
+           <Navbar/>
+        </div>
+      
+      {/* Page content */}
+      <div className="p-8">
+        <h1>Page Content</h1>
+      </div>
+    </div>
     </>
   )
 }
